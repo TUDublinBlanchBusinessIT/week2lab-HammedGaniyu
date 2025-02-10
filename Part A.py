0db996c3c467327ca4be38ef774a9106be3f0db6
HammedGaniyu <B00167256@mytudublin.ie>
2025-02-10 17:46:14 +0000

    Completed Part A

---
+++ Part A.py
@@ -0,0 +1,22 @@
+#Hammed Ganiyu
+#10/02/2025
+#A Program to Print Steps 1 to 9 to the Screen
+
+
+def printSteps():
+    print("Step 4")
+    print("Step 5")
+    print("Step 6")
+
+
+
+def main():
+    printSteps()
+
+main()
+
+
+
+
+
+
