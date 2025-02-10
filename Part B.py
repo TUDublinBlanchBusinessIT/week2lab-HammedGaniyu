abb62a2811f93adc7148cd0048e8df8472b25582
HammedGaniyu <B00167256@mytudublin.ie>
2025-02-10 20:30:17 +0000

    Completed Part B

---
+++ Part B.py
@@ -0,0 +1,20 @@
+#Hammed Ganiyu
+#10/2/2025
+# Part B
+
+def printSteps(a,b):
+    for i in range(a,b):
+        print(f"Step {i}")
+
+
+
+        
+
+
+def main():
+    a = 1
+    b = 10
+    printSteps(a,b)
+
+
+main()
