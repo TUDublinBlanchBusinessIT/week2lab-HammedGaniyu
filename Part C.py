e98c983c903a4fb2a23ee73adef400ddc305ff14
HammedGaniyu <B00167256@mytudublin.ie>
2025-02-10 21:42:03 +0000

    Completed Part B

---
+++ Part C.py
@@ -0,0 +1,21 @@
+#Hammed Ganiyu
+#10/02/2025
+#Part C
+
+
+def printSteps(a,b):
+    for i in range(a,b +1):
+        print(f"Step {i}")
+
+
+
+        
+
+
+def main():
+    a = int(input("Please enter first step: "))
+    b = int(input("Please enter last step: "))
+    printSteps(a,b)
+
+
+main()
